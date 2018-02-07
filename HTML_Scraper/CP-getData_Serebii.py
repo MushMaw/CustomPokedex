dddb86df79870c4dc58d1a61e8a7105c27175d18
MushMaw <jttbakker@gmail.com>
2018-02-07 16:02:50 -0700

    Added G.Percentages and a few others

--- HTML_Scraper/CP-getData_Serebii.py
+++ HTML_Scraper/CP-getData_Serebii.py
@@ -6,29 +6,125 @@ Module: getData (Serebii)
 
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
-from CP-PokemonClass import Pokemon
+from CP_PokemonClass import Pokemon
 import re
 
 
-def getAllPokemon(url):
+def getInfoForPokemon(url, newMon):
 	resp = openURL(url)
 	if (resp == -1):
 		return resp
-	PokemonDict = {}
-	
-	gatherTableInfo(resp, PokemonDict)
+	print("Connected")
+	gatherTableInfo(resp, newMon)
 		
 def gatherTableInfo(resp, PokemonDict):
 	soup = BeautifulSoup(resp, "lxml")
 	
-	dexTableTag = soup.find("table", {"class":"dextable"})
-	parseDexTable(dexTableTag, PokemonDict)
+	firstDexTableTag = soup.find("table", {"class":"dextable"})
+	parseDexTable(firstDexTableTag, PokemonDict)
 	
+def parseDexTable(dexTableTag, newMon):
+	tableRow = dexTableTag.find("tr").find_next_sibling()
+	checkRow = tableRow.find("td").find_next_sibling()
+	newMon.name = checkRow.getText()
+	
+	checkRow = tableRow.find("td", {"class":"cen"})
+	getTypesForForms(checkRow, newMon.name, newMon.types)
+	for form in newMon.types:
+		newMon.forms.append(form)
+		
+	checkRow = checkRow.find_previous_sibling()
+	getGenderChances(checkRow, newMon.genderChances)
+	
+	tableRow = findSibling_n(tableRow, 2, None)
+	td_Tag = tableRow.find("td")
+	newMon.category = td_Tag.getText()
+	td_Tag = td_Tag.find_next_sibling()
+	getStature(td_Tag, newMon.forms, newMon.height, "h")
+	td_Tag = td_Tag.find_next_sibling()
+	getStature(td_Tag, newMon.forms, newMon.weight, "w")
+	
+def getStature(checkTag, formList, outputDict, mode):
+	baseString = checkTag.getText()
+	print(baseString)
+	n = len(formList)
+	formatList = []
+	if (mode == "h"):
+		formatList = Pokemon.HEIGHT_FORMATS
+	else:
+		for measure in Pokemon.WEIGHT_MEASURES:
+			formatList.append(Pokemon.WEIGHT_FORMAT + measure)
+	for count, format in enumerate(formatList):
+		regExp = HW_buildRegExp(format, n)
+		m = re.search(regExp, baseString)
+		for i in range(n):
+			key = formList[i]
+			value = m.group(i+1)
+			if (mode == "w"):
+				value += Pokemon.WEIGHT_MEASURES[count]
+			if (key not in outputDict):
+				outputDict[key] = [value]
+			else:
+				outputDict[key].append(value)
 
-def parseDexTable(dexTableTag, PokemonDict):
-	
+def HW_buildRegExp(regUnit, n):
+	spacing = " / "
+	regExp = regUnit
+	for i in range(n-1):
+		regExp += (spacing + regUnit)
+	return regExp
 
+def getTypesForForms(checkRow, mainName, typeDict):
+	allTypeRows = [checkRow.find("tr")]
+	if (allTypeRows[0] != None):
+		allTypeRows += allTypeRows[0].find_next_siblings()
+		for tRow in allTypeRows:
+			tdTag = tRow.find("td")
+			formName = tdTag.getText()
+			allTypes = getEachTypeForRow(tdTag.find_next_sibling())
+			typeDict[formName] = allTypes
+	else:
+		allTypes = getEachTypeForRow(checkRow)
+		typeDict[mainName] = allTypes
+	return typeDict
+
+def getGenderChances(checkRow, genderDict):
+	genderRow = checkRow.find("tr")
+	if (genderRow != None):
+		eachGender = [genderRow] + genderRow.find_next_siblings()
+		for g in eachGender:
+			g_td = g.find("td")
+			genderName = g_td.find(text=True, recursive=False)
+			genderChance = g_td.find_next_sibling().getText()
+			genderDict[genderName] = genderChance
+	return genderDict
 	
+def findSibling_n(soup, n, tag):
+	end = soup
+	while (n > 0):
+		if (tag != None):
+			next = end.find_next_sibling(tag)
+		else:
+			next = end.find_next_sibling()
+		if (next == None):
+			break
+		else:
+			end = next
+		n -= 1
+	if (end == soup):
+		return None
+	return end
+	
+def getEachTypeForRow(checkRow):
+	allTypes = []
+	allTypeTags = checkRow.findAll("a")
+	typeRegExp = "/(\w+).gif"
+	for typeTag in allTypeTags:
+		typeText = typeTag.find("img")["src"]
+		type = re.search(typeRegExp, typeText).group(1)
+		allTypes.append(type)
+	return allTypes
+
 def openURL(url):
 	maxAttempts = 15
 	while (maxAttempts > 0):
@@ -41,4 +137,10 @@ def openURL(url):
 	return -1
 
 if (__name__ == "__main__"):
-	return 0
+	for i in range(5):
+		dexNum = 2
+		newMon = Pokemon()
+		newMon.dexNumber = dexNum
+		url = "https://www.serebii.net/pokedex-sm/" + newMon.dexToString() + ".shtml"
+		getInfoForPokemon(url, newMon)
+		newMon.printThis()
---
+++ HTML_Scraper/CP_PokemonClass.py
@@ -0,0 +1,49 @@
+'''
+
+'''
+
+class Pokemon:
+	STAT_NAMES = ["hp", "attack", "defense", "sp. attack", "sp. defense", "speed"]
+	BASE_FILE_NAME = "pokemonObject_"
+	HEIGHT_IMPERIAL_FORMAT = '''([0-9]+'[0-9]+")'''
+	DECIMAL_FORMAT = "([0-9]+.*[0-9]*)"
+	HEIGHT_METRIC = "m"
+	WEIGHT_MEASURES = ["lbs", "kg"]
+	
+	def __init__(self):
+		# Dextable info
+		self.name = ""
+		self.category = ""
+		self.forms = []
+		self.types = {}
+		self.genderChances = {}
+		self.dexNumber = 0
+		self.height = {}
+		self.weight = {}
+		self.eggSteps = {}
+		self.captureRate = {}
+		
+		self.expRate = ""
+		self.maxExp = -1
+		self.expYield = -1
+		self.evYield = {}
+		for stat in Pokemon.STAT_NAMES:
+			self.evYield[stat] = 0
+			
+		self.eggGroup = ""
+		self.baseHappiness = -1
+		
+	def dexToString(self):
+		output = str(self.dexNumber)
+		while (len(output) < 3):
+			output = "0" + output
+		return output
+		
+	def printThis(self):
+		print(self.name + "  |  " + self.category + "  |  #" + self.dexToString())
+		for each in self.forms:
+			print(each + " : ", self.types[each])
+		for each in self.genderChances:
+			print(each + " : " + self.genderChances[each])
+		
+		
